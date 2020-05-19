0b06581311e240d5cb784b302f96b7dca1adc6f9
apriyogadaulay <65588555+apriyogadaulay@users.noreply.github.com>
2020-05-19 15:15:38 +0700

    Update index.php

--- Modul5/index.php
+++ Modul5/index.php
@@ -83,7 +83,7 @@ var theme = {
 // tui.chart.registerTheme('myTheme', theme);
 // options.theme = 'myTheme';
 
-tui.chart.pieChart(container, data, options);
+tui.chart.lineChart(container, data, options);
 </script>
 
 <!--For tutorial page-->
